14b82d0b8e80c197e2e7581108692ddb2f8cf257
Duclaux Fredy <tagneduclaux@gmail.com>
2026-02-10 03:08:22 +0100

    Adding security

---
+++ src/main/java/fox/dev/todo_management/config/SpringSecurityConfig.java
@@ -0,0 +1,18 @@
+package fox.dev.todo_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf((csrf) -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
